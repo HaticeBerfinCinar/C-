a5ab256fb09fa46363d8a9520b1589e0cf49d569
example <email@example.com>
2019-10-22 17:34:52 +0300

    msg

---
+++ GitHub3.java
@@ -0,0 +1,18 @@
+package GitHub;
+
+public class GitHub3 {
+
+	public static void main(String[] args) {
+
+        int sayi = 25, bolen = 4;
+ 
+        int bolum = sayi / bolen;
+        int kalan = sayi % bolen;
+ 
+        System.out.println("Ýþlem:"+sayi+"/"+bolen);
+        System.out.println("Bölüm = " + bolum);
+        System.out.println("Kalan = " + kalan);
+
+	}
+
+}
